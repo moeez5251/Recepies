93029371ede0690b1b82fa4727d821f6cca95ee9
moeez5251 <moeez66656@gmail.com>
2024-08-27 17:04:03 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '6f3d356613msh297728a391c575ap149f05jsnf273894f8b95',
+// 		'x-rapidapi-host': 'tasty.p.rapidapi.com'
+// 	}
+// };
+// async function main() {
+
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// main();
+
+window.addEventListener("load", () => {
+    document.querySelector("nav").style.marginTop = "22px";
+    setTimeout(() => {
+        document.querySelector("nav").style.width = "40%";
+    }, 800);
+  
+})
+
+let span=document.querySelectorAll(".type span");
+function scroll(){
+    if (window.scrollY>150) {
+        for(let i=0;i<=500;i++){
+            setTimeout(() => {
+                span[0].innerHTML=i+"+";
+            }, i*3);
+        }
+        for(let i=0;i<=1000;i++){
+            setTimeout(() => {
+                span[1].innerHTML=i+"+";
+            }, i*1);
+        }
+        for(let i=0;i<=200;i++){
+            setTimeout(() => {
+                span[2].innerHTML=i+"+";
+            }, i*10);
+            window.removeEventListener("scroll",scroll)
+        }
+    }
+}
+window.addEventListener("scroll",scroll)
